006c0abfc5e8cc0e01b5949b2cb66067d7b12c03
Joshua Chi <jchi@thenetcircle.com>
2013-12-17 14:40:09 +0800

    add multicast demo

---
+++ multicast/js/main.js
@@ -0,0 +1,94 @@
+MultiCast = {
+  connection: null,
+  _onMessage: function(msg) {
+              console.log(msg);
+              var elements = msg.getElementsByTagName('body');
+              if (0 < elements.length) {
+                var data = elements[0].text || elements[0].textContent;
+                ary = data.split('#');
+                if (0 < ary.length) {
+                  if ('multicast' == ary[0]){
+                    if('available' == ary[2]){
+                      console.log(ary[1]+"available");
+                      $('#fav_'+ary[1]+ ' > span').removeClass('disabled').removeClass('offline').addClass('online');
+                    }else{
+                      console.log(ary[1]+"unavailable");
+                      $('#fav_'+ary[1]+ ' > span').removeClass('online').removeClass('disabled').addClass('offline');
+                    }                    
+                  }
+                }
+              }
+              // We must return true to keep the handler alive.
+              // Returning false would remove it after it finishes.
+              return true;
+            },
+  _onPresence: function(pres) {
+               // console.log(pres);
+               // var elements = pres.getElementsByTagName('show');
+               // if (0 < elements.length) {
+               //   var data = elements[0].text || elements[0].textContent;
+               //   var to = pres.getAttribute("to");
+               //   ary = to.split('@');
+               //   if('available' == data){
+               //      $('#'+ary[0]).removeClass('inactive').addClass('active');
+               //   }else{
+               //      $('#'+ary[0]).removeClass('active').addClass('inactive');
+               //   }
+               // }
+             },
+};
+
+$(document).ready(function () {
+  $('#login_dialog').dialog({
+    autoOpen: true,
+    draggable: false,
+    model: true,
+    title: 'Connect to XMPP',
+    buttons: {
+      "Connect": function () {
+        $(document).trigger('connect', {
+          jid: $('#jid').val()+"@master.joshua.kaufmich.lab",
+          password: $('#password').val()
+        });
+        $('#password').val('');
+        $(this).dialog('close');
+      }
+    }
+  });
+  $('#disconnect').click(function () {
+    $('#disconnect').attr('disabled', 'disabled');
+    MultiCast.connection.disconnect();
+  });
+});
+
+$(document).bind('connect', function (ev, data) {
+    var conn = new Strophe.Connection('http://pubsub.lab:5280/http-bind');
+    conn.connect(data.jid, data.password, function (status) {
+      if (status == Strophe.Status.CONNECTING) {
+        console.log('Connecting...');
+      } else if (status == Strophe.Status.CONNFAIL) {
+        console.log('Failed to connect!');
+      } else if (status == Strophe.Status.DISCONNECTING) {
+        console.log('Disconnecting...');
+      } else if (status == Strophe.Status.DISCONNECTED) {
+        console.log('Disconnected');
+        $(document).trigger('disconnected');
+      } else if (status == Strophe.Status.CONNECTED) {
+        console.log('Connected');
+        $("#iam").text(data.jid);
+        $(document).trigger('connected');
+      }
+      return true;
+    });
+    MultiCast.connection = conn;
+});
+$(document).bind('connected', function () {
+    $('#disconnect').parent().removeClass('disabled');
+    MultiCast.connection.send($pres());
+    MultiCast.connection.addHandler(MultiCast._onMessage, null, 'message', null, null,  null);
+    MultiCast.connection.addHandler(MultiCast._onPresence, null, 'presence', null, null,  null);
+});
+$(document).bind('disconnected', function () {
+    MultiCast.connection = null;
+    $('#login_dialog').dialog('open');
+});
